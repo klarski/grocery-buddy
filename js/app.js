9be280def97f5d9d4e80722e23906b51a09cc9d4
klarski <kristennl1@gmail.com>
2014-03-05 23:48:27 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+function DBController ($scope){
+	$scope.itemName;
+
+	$scope.listArray = ["Bread", "Eggs", "Milk", "Flour"];
+
+	$scope.addItem = function(){
+		if($scope.itemName.length > 1){
+		$scope.listArray.push($scope.itemName);
+
+		$scope.itemName = ""; 
+		}else{
+
+		}
+
+	};
+
+	$scope.deleteItem = function(deletedItem){
+		var ind = $scope.listArray.indexOf(deletedItem);
+		$scope.listArray.splice(ind, 1);
+	};
+
+};
