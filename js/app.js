cffc321ed2096b769eccdc7705cfac0b242b4e96
klarski <kristennl1@gmail.com>
2014-03-08 19:50:46 -0500

    add form validation

--- js/app.js
+++ js/app.js
@@ -2,17 +2,21 @@ function DBController ($scope){
 	$scope.itemName;
 
 	$scope.listArray = ["Bread", "Eggs", "Milk", "Flour"];
+	
 
 	$scope.addItem = function(){
-		$scope.listArray.push($scope.itemName);
 
-		$scope.itemName = ""; 
+		if($scope.itemName.length > 1){
 
-	};
+				$scope.listArray.push($scope.itemName);
+
+				$scope.itemName = ""; 
+		}
+	}
 
 	$scope.deleteItem = function(deletedItem){
 		var ind = $scope.listArray.indexOf(deletedItem);
 		$scope.listArray.splice(ind, 1);
-	};
+	}
 
-};
+}
